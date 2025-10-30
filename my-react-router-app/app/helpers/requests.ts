0b27b2c296b86e54f6766b11da473e16cb3abcce
Alfie135 <182797639+Alfie135@users.noreply.github.com>
2025-10-30 15:46:38 +0000

    server stuff
    no idea, server stuff

---
+++ my-react-router-app/app/helpers/requests.ts
@@ -0,0 +1,15 @@
+export async function getData() {
+  const url = "http://localhost:3000";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    return result.message;
+  } catch (error: any) {
+    if (error && error.message)
+    console.error(error.message);
+  }
+}
--- my-react-router-app/app/routes/home.tsx
+++ my-react-router-app/app/routes/home.tsx
@@ -1,7 +1,8 @@
 import Counter from "../components/Counter/counter";
 import type { Route } from "./+types/home";
+import Server from "~/components/server";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
     { name: "description", content: "Welcome to React Router!" },
@@ -9,5 +10,8 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Counter/>;
+  return <div>
+    <Counter />
+    <Server />
+  </div>;
 }
